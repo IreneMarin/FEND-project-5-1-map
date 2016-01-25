58a4230048fbe740771926f69e53b168036e7a25
Irene Marin <irene@marin.cat>
2016-01-25 11:19:41 +0100

    feat: Create basic structure

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'css/app.css': 'sass/app.scss',
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('default', ['sass']);
+};
---
+++ js/app.js
@@ -0,0 +1,48 @@
+// Google Maps API
+var map;
+var infowindow;
+
+function initMap() {
+  var barcelona = {lat: 41.4029277, lng: 2.157448};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: barcelona,
+    zoom: 15
+  });
+
+  infowindow = new google.maps.InfoWindow();
+
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: barcelona,
+    radius: 500,
+    types: ['store']
+  }, callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+
+var ViewModel = function () {
+
+}
+ko.applyBindings(new ViewModel());
