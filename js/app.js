071dbcdf821b4ea9c9d5b0f3de1d9078db7412c3
Irene Marín <irene@marin.cat>
2016-03-14 09:04:24 +0100

    feat: Show calendar and sidebar

--- js/app.js
+++ js/app.js
@@ -1,48 +1,8 @@
-// Google Maps API
+// Create Google Maps and initialize its center in the Vila de Gracia neighborhood
 var map;
-var infowindow;
-
 function initMap() {
-  var barcelona = {lat: 41.4029277, lng: 2.157448};
-
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: barcelona,
-    zoom: 15
-  });
-
-  infowindow = new google.maps.InfoWindow();
-
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch({
-    location: barcelona,
-    radius: 500,
-    types: ['store']
-  }, callback);
-}
-
-function callback(results, status) {
-  if (status === google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-    }
-  }
-}
-
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
-
-
-var ViewModel = function () {
-
-}
-ko.applyBindings(new ViewModel());
+	map = new google.maps.Map(document.getElementById('map'), {
+    	center: { lat: 41.4029277, lng: 2.157448 },
+    	zoom: 16
+	});
+} 
---
+++ js/places.js
@@ -0,0 +1,153 @@
+var places = [{
+    place: 'restaurant',
+    name: "Messie Pizza",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude:      
+}, {
+    place: 'restaurant',
+    name: "Take-ya",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude:        
+}, {
+    place: 'restaurant',
+    name: "Kibuka Goya",
+    address: "Carrer de Goya, 9, 08012, Barcelona",
+    phone: "93 237 89 94",
+    web: "kibuka.com",
+    latitude: 41.4002376,
+    longitude: 2.1585076        
+}, {
+    place: 'restaurant',
+    name: "Restaurant Nomo",
+    address: "Carrer Gran de Gràcia, 13, 08012, Barcelona",
+    phone: "93 415 96 22",
+    web: "restaurantenomo.com",
+    latitude: 41.3999902,
+    longitude: 2.1592873    
+}, {
+    place: 'restaurant',
+    name: "L'Olla del Torrent",
+    address: "Carrer del Torrent de l'Olla, 106, 08012, Barcelona",
+    phone: "93 210 49 01",
+    web: "lolladeltorrent.com",
+    latitude: 41.4014729,
+    longitude: 2.1570592
+}, {
+    place: 'bar',
+    name: "Bar El Otro",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude: 
+}, {
+    place: 'bar',
+    name: "Bar St. Germain",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude: 
+}, {
+    place: 'coffeeshop',
+    name: "El Suís",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude:
+}, {
+    place: 'library',
+    name: "Biblioteca Vila de Gràcia",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude: 
+}, {
+    place: 'books',
+    name: "La Memòria",
+    address: "Plaça de la Vila de Gràcia, 19, 08012, Barcelona",
+    phone: "93 624 36 32",
+    web: "",
+    latitude: 41.4001503,
+    longitude: 2.157419
+}, {
+    place: 'books',
+    name: "Haiku",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude:
+}, {
+    place: 'comicbooks',
+    name: "Antifaz",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude:
+}, {
+    place: 'games',
+    name: "Game.es",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude:
+}, {
+    place: 'games',
+    name: "",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude:
+}, {
+    place: 'cinema',
+    name: "Cines Verdi",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude:
+}, {
+    place: 'cinema',
+    name: "Cinemes Girona",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude:
+}, {
+    place: 'theatre',
+    name: "Teatre Lliure",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude:
+}, {
+    place: 'theatre',
+    name: "Teatreneu",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude:
+}, {
+    place: '',
+    name: "",
+    address: "",
+    phone: "",
+    web: "",
+    latitude: ,
+    longitude:
+}];
