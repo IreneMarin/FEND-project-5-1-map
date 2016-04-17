a0ed1f59f077437c4aa9a17382455360ccb1d4ce
Irene Marín <irene@marin.cat>
2016-04-17 13:02:17 +0200

    fix: Start again with Map and lists

--- js/app.js
+++ js/app.js
@@ -1,8 +1,130 @@
-// Create Google Maps and initialize its center in the Vila de Gracia neighborhood
+// Create variables used in the app
 var map;
+var placesListLength = places.length;
+
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-    	center: { lat: 41.4029277, lng: 2.157448 },
-    	zoom: 16
-	});
-} 
+
+    // Initialize Google Maps
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 41.4029277, lng: 2.157448 },
+        zoom: 16
+    });
+
+    // Put list of places in the map
+    for (var i = 0; i < placesListLength; i++) {
+        var place = places[i];
+        var marker = new google.maps.Marker({
+            animation: google.maps.Animation.DROP,
+            map: map,
+            position: new google.maps.LatLng(places[i].latitude, places[i].longitude),
+            name: places[i].name
+        });
+        place.marker = marker;
+    }
+
+
+
+}
+
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+
+function ViewModel() {
+    var self = this;
+
+    self.locations = ko.observableArray([]);
+
+    for (var i = 0; i < placesListLength; i++) {
+        self.locations.push(places[i]);
+        console.log(places[i]);
+    }
+
+    console.log(self.locations);
+
+    // aquí dins només lo estricte de knowckout
+    // tot el mapa fora 
+
+
+}
+ko.applyBindings(new ViewModel());
+
+/*
+var ViewModel = function() {
+
+    infowindow = new google.maps.InfoWindow();
+    var listLocationsLength = places.length;
+
+    // Placing the icons on the map for every hard-coded location.
+
+    for (var i = 0; i < listLocationsLength; i++) {
+
+        // Changing the icon to a corresponding flag. Along with the size of the icons on the map.        
+        var location = places[i];
+        var latLng = new google.maps.LatLng(places[i].latitude, places[i].longitude);
+        var marker = new google.maps.Marker({
+            animation: google.maps.Animation.DROP,
+            map: map,
+            position: latLng,
+            name: places[i].name
+        });
+        //tagList.push(location.name); // Taglist added to be used by auto-complete.
+        location.marker = marker;
+
+        marker.addListener('visible_changed', function() { // Added in resubmission, to hide the marker when its visibility changes
+            infowindow.close(map, marker);
+        });
+
+        // Opening infowindows on clicking the hard-coded markers.
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+
+            return function() {
+                infowindow.open(map, marker);
+                toggleBounce(marker);
+                loadData(marker, infowindow);
+                loadPhoto(marker, infowindow);
+            };
+
+            // Animating the markers when clicked
+
+            function toggleBounce(marker) {
+                if (marker.getAnimation() !== null) {
+                    marker.setAnimation(null);
+                } else {
+                    marker.setAnimation(google.maps.Animation.BOUNCE);
+                    setTimeout(stopBounce, 2100); // Exactly 3 bounces when clicking
+
+                    function stopBounce() {
+                        marker.setAnimation(null);
+                    }
+                }
+            }
+        })(marker, i));
+
+    }
+    
+}
+ko.applyBindings(new ViewModel());
+
+*/
+
+
+/*
+var ViewModel = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Taby');
+    this.imgSrc = ko.observable('img/xxxx.jpg');
+    
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
+}
+
+ko.applyBindings (new ViewModel());
+*/
---
+++ js/locations.js
@@ -0,0 +1,89 @@
+var places = [{
+    category: 'restaurant',
+    icon: 'fa fa-cutlery',
+    name: 'Messie Pizza',
+    address: 'Torrent de Olla, 65',
+    web: 'http://messiepizza.es',
+    latitude: 41.4009036,
+    longitude: 2.1565914
+}, {   
+    category: 'restaurant',
+    icon: 'fa fa-cutlery',
+    name: 'Kibuka Goya',
+    address: 'Goya, 9',
+    web: 'http://kibuka.com',
+    latitude: 41.4002376,
+    longitude: 2.1585076        
+}, {   
+    category: 'restaurant',
+    icon: 'fa fa-cutlery',
+    name: 'Olla del Torrent',
+    address: 'Torrent de Olla, 106',
+    web: 'http://lolladeltorrent.com',
+    latitude: 41.4014729,
+    longitude: 2.1570592
+}, {
+    category: 'bar',
+    icon: 'fa fa-beer',
+    name: 'Bar El Otro',
+    address: 'Travessera de Gràcia, 167',
+    web: 'http://barelotro.com',
+    latitude: 41.401468,
+    longitude: 2.155701
+}, {
+    category: 'bar',
+    icon: 'fa fa-beer',
+    name: 'Bar Vinil()',
+    address: 'Matilde, 2',
+    web: 'https://ca-es.facebook.com/vinil-bar-54874665280/',
+    latitude: 41.4001986,
+    longitude: 2.1571333
+}, {
+    category: 'bar',
+    icon: 'fa fa-beer',
+    name: 'Bar St. Germain',
+    address: 'Torrent de Olla, 113',
+    web: 'https://ca-es.facebook.com/St-Germain-117565731655693/',
+    latitude: 41.4714437,
+    longitude: 2.1146087
+}, {    
+    category: 'books',
+    icon: 'fa fa-book',
+    name: 'La Memòria',
+    address: 'Plaça de la Vila de Gràcia, 19',
+    web: 'https://www.facebook.com/llibreria.lamemoria/',
+    latitude: 41.4001503,
+    longitude: 2.157419
+}, {
+    category: 'books',
+    icon: 'fa fa-book',
+    name: 'Haiku',
+    address: 'Montseny, 7',
+    web: 'http://haikubarcelona.com',
+    latitude: 41.4009524,
+    longitude: 2.1565492
+}, {
+    category: 'books',
+    icon: 'fa fa-book',
+    name: 'Antifaz Comics',
+    address: 'Gran de Gràcia, 239',
+    web: 'http://antifazcomic.cat',
+    latitude: 41.4053551,
+    longitude: 2.148452
+}, {
+    category: 'games',
+    icon: 'fa fa-gamepad',
+    name: 'Game.es',
+    address: 'Gran de Gràcia, 116',
+    web: 'http://game.es',
+    latitude: 41.3989547,
+    longitude: 2.1496657
+}, {
+    category: 'games',
+    icon: 'fa fa-gamepad',
+    name: 'The Curiosity Shop',
+    address: 'Ramon y Cajal, 13',
+    web: 'http://thecuriosityshop.es',
+    latitude: 41.4009263,
+    longitude: 2.1504057
+}];
