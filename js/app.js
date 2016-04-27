5e74eeab5e3902b75f6b2ff732ec166c1c3c3072
Irene Marín <irene@marin.cat>
2016-04-27 23:50:34 +0200

    feat: Create list filter in KO

--- js/app.js
+++ js/app.js
@@ -8,35 +8,71 @@ var placesListLength = places.length;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 41.4029277, lng: 2.157448 },
-        zoom: 16
+        zoom: 17
     });
 
     // Put list of places in the map
-    for (var i = 0; i < placesListLength; i++) {        
+    /*for (var i = 0; i < placesListLength; i++) {        
         var marker = new google.maps.Marker({
             animation: google.maps.Animation.DROP,
             map: map,
             position: new google.maps.LatLng(places[i].latitude, places[i].longitude),
             name: places[i].name
         });
-    }
+    }*/
 }
 
 // Function to toggle the map markers
+/*
 function toggleBounce() {
     if (marker.getAnimation() !== null) {
         marker.setAnimation(null);
     } else {
         marker.setAnimation(google.maps.Animation.BOUNCE);
     }
-}
+}*/
 
+
+
+var viewModel = {
+  
+  // Put all the places in the list by default
+  places: ko.observableArray(places.slice(0)),
+  
+  // Observable for the search. Empty because we haven't searched anything yet
+  query: ko.observable(''),
+  
+  search: function(value) {
+    // Remove all the current places, which removes them from the view
+    viewModel.places.removeAll();
+
+    for(var i = 0; i < placesListLength; i++) {
+      if (places[i].name.toLowerCase().indexOf(value.toLowerCase()) >= 0 
+      || places[i].category.toLowerCase().indexOf(value.toLowerCase()) >= 0)  {
+        viewModel.places.push(places[i]);
+      }
+    }
+  }
+  
+};
+
+viewModel.query.subscribe(viewModel.search);
+
+ko.applyBindings(viewModel);
+
+
+
+
+
+/*
 // ViewModel Knockout
 function ViewModel() {
     var self = this;
 
     // Put all the places in the list by default
     self.places = ko.observableArray([]);
+    
+    self.places = ko.observableArray(places)
 
     for (var i = 0; i < placesListLength; i++) {
         self.places.push(places[i]);        
@@ -46,10 +82,46 @@ function ViewModel() {
         console.log('hi');
         // aquí posarem el toggle per fer que es vergi nomes el marker aquest 
     };     
+    
+    
+    
+    // Observable for the search. Empty because we haven't searched anything by default
+    self.query = ko.observable(''); 
+
+    self.search = function(value) {
+        // remove all the current beers, which removes them from the view
+        viewModel.places.removeAll();
+
+        for(var x in places) {
+            if(places[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+                self.places.push(places[x]);
+            }
+        }
+    }
+    
+    
+    self.results = ko.computed(function () {
+        return ko.utils.arrayFilter(self.places(), function (list) {
+            //Match search with items in sortedLocations() observable array
+            var listFilter = list.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+            if (listFilter) { //if user input matches any of the brewery names, show only the matches
+                toggleMarker.marker.setVisible(true);
+            } else {
+                toggleMarker.marker.setVisible(false); //hide markers and list items that do not match results
+            }
+
+            return listFilter;
+
+        });
+    });
+   
+    
 }
 
-ko.applyBindings(new ViewModel());
+viewModel.query.subscribe(viewModel.search);
 
+ko.applyBindings(new ViewModel());
+*/
 
 
 /*
--- js/places.js
+++ js/places.js
@@ -22,6 +22,38 @@ var places = [{
     web: 'http://lolladeltorrent.com',
     latitude: 41.4014729,
     longitude: 2.1570592
+}, {   
+    category: 'restaurant',
+    icon: 'fa fa-cutlery',
+    name: 'Restaurant Toyo',
+    address: 'Torrent de Olla, 10',
+    web: 'http://restaurantetoyo.com',
+    latitude: 41.3987222,
+    longitude: 2.1593477
+}, {   
+    category: 'restaurant',
+    icon: 'fa fa-cutlery',
+    name: 'Ramen-ya Hiro',
+    address: 'Girona, 164',
+    web: '',
+    latitude: 41.3996514,
+    longitude: 2.1631275
+}, {   
+    category: 'restaurant',
+    icon: 'fa fa-cutlery',
+    name: 'Al Waha',
+    address: 'Verdi, 16',
+    web: '',
+    latitude: 41.403459,
+    longitude: 2.1567388
+}, {   
+    category: 'restaurant',
+    icon: 'fa fa-cutlery',
+    name: 'I què?',
+    address: 'Topazi, 8',
+    web: 'http://iquerestaurant.com',
+    latitude: 41.4045422,
+    longitude: 2.154889
 }, {
     category: 'bar',
     icon: 'fa fa-beer',
@@ -46,7 +78,47 @@ var places = [{
     web: 'https://ca-es.facebook.com/St-Germain-117565731655693/',
     latitude: 41.4714437,
     longitude: 2.1146087
-}, {    
+}, {
+    category: 'bar',
+    icon: 'fa fa-beer',
+    name: 'La Maceta',
+    address: 'Progrés, 38',
+    web: '',
+    latitude: 41.4005163,
+    longitude: 2.1592543
+}, {
+    category: 'coffee-shop',
+    icon: 'fa fa-coffee',
+    name: 'Cafe Camelia',
+    address: 'Verdi, 79',
+    web: '',
+    latitude: 41.4051147,
+    longitude: 2.1548722
+}, {
+    category: 'coffee-shop',
+    icon: 'fa fa-coffee',
+    name: 'Oh! Caffe',
+    address: 'Gran de Gràcia',
+    web: '',
+    latitude: 41.3989692,
+    longitude: 2.1476088
+}, {
+    category: 'coffee-shop',
+    icon: 'fa fa-coffee',
+    name: 'Cafè Godot',
+    address: 'Sant Domènec, 19',
+    web: 'http://cafegodot.com',
+    latitude: 41.4001678,
+    longitude: 2.1564141
+}, {
+    category: 'coffee-shop',
+    icon: 'fa fa-coffee',
+    name: 'Cafetería El Suís',
+    address: 'Travessera de Gràcia, 151',
+    web: 'http://elsuis.com',
+    latitude: 41.4009243,
+    longitude: 2.156545
+}, {
     category: 'books',
     icon: 'fa fa-book',
     name: 'La Memòria',
@@ -62,6 +134,14 @@ var places = [{
     web: 'http://haikubarcelona.com',
     latitude: 41.4009524,
     longitude: 2.1565492
+}, {
+    category: 'books',
+    icon: 'fa fa-book',
+    name: 'Aldarull',
+    address: 'Torrent de Olla, 72',
+    web: 'http://acciocultural.org',
+    latitude: 41.4005898,
+    longitude: 2.1570969
 }, {
     category: 'books',
     icon: 'fa fa-book',
@@ -86,4 +166,60 @@ var places = [{
     web: 'http://thecuriosityshop.es',
     latitude: 41.4009263,
     longitude: 2.1504057
+}, {
+    category: 'games',
+    icon: 'fa fa-gamepad',
+    name: 'Roomin Escape',
+    address: 'Robí, 6',
+    web: '',
+    latitude: 41.404547,
+    longitude: 2.1547458
+}, {
+    category: 'cinema & theatre',
+    icon: 'fa fa-video-camera',
+    name: 'Teatreneu',
+    address: 'Terol, 26',
+    web: 'http://teatreneu.com',
+    latitude: 41.4032351,
+    longitude: 2.1564512
+}, {
+    category: 'cinema & theatre',
+    icon: 'fa fa-video-camera',
+    name: 'Teatre Lliure de Gràcia',
+    address: 'Montseny, 47',
+    web: 'http://teatrelliure.com',
+    latitude: 41.4027398,
+    longitude: 2.1538444
+}, {
+    category: 'cinema & theatre',
+    icon: 'fa fa-video-camera',
+    name: 'Cinemes Girona',
+    address: 'Girona, 175',
+    web: 'http://cinemesgirona.cat',
+    latitude: 41.399656,
+    longitude: 2.1623361
+}, {
+    category: 'cinema & theatre',
+    icon: 'fa fa-video-camera',
+    name: 'Cine Texas',
+    address: 'Bailèn, 205',
+    web: 'http://cinemestexas.cat',
+    latitude: 41.4020494,
+    longitude: 2.1614495
+}, {
+    category: 'cinema & theatre',
+    icon: 'fa fa-video-camera',
+    name: 'Bosque Multicines',
+    address: 'Rambla del Prat, 16',
+    web: 'http://grupbalana.com',
+    latitude: 41.4015154,
+    longitude: 2.1496618
+}, {
+    category: 'cinema & theatre',
+    icon: 'fa fa-video-camera',
+    name: 'Cine Verdi',
+    address: 'Torrijos, 14',
+    web: 'http://cines-verdi.com',
+    latitude: 41.4040056,
+    longitude: 2.1546641
 }];
